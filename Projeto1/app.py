b616207ff298b2400fbbfc39fd9b4896b1d28916
Eurico <euricofsv@gmail.com>
2025-02-02 02:29:27 +0000

    ajustar a ultima parte do codigo para funcionar no render

--- Projeto1/app.py
+++ Projeto1/app.py
@@ -149,5 +149,12 @@ def processar_parquet():
     )
 
 # Executar a aplicação
+#if __name__ == "__main__":
+    #app.run(debug=True)
+
+#colocar este pedaço de codigo para funcionar no render
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
